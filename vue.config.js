451803373a9417f090e4d7c55ab9958774366a3e
parkyoungjun <syyjbhjh7@naver.com>
2023-02-27 15:55:34 +0900

    Update : Create Vue + Spring boot dev environment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  outputDir: "../../src/main/resources/static",
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8086/',
+        changeOrigin: true
+      }
+    }
+  }
+};
